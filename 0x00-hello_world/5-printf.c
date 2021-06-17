c51783e54595377ea847d24ec8367365bc73c41b
Elijahdanie <elijahdanie@gmail.com>
2021-06-17 13:04:11 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 
 /**
-* Description - This Script is meant to print this statement with a new line
-* "with proper grammar, but the outcome is a piece of art"
-* main(void)-this is the entry point and the void passed in is similar to a null parameter
-* return(0) This indicates success in code execution
+* main - Entry point
+*
+* Description : prints the indicated quote to the console
+*
+* Return: Always 0 (Success)
 */
 
 
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,/n");
+return (0);
 }
