06413542b1d324041e942426dacd5dc7af36c76e
Elijahdanie <elijahdanie@gmail.com>
2021-06-17 12:09:24 -0700

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -7,8 +7,9 @@ include <stdio.h>
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
+  puts("Programming is like building a multilingual puzzle");
   return (0);
 }
