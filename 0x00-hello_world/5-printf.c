d5104f9463e5e871cea95538ac3590b5625dbac9
Elijahdanie <elijahdanie@gmail.com>
2021-06-17 12:43:01 -0700

    added 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 
-/*
- *Description - This Script is meant to print this statement with a new line
- *"with proper grammar, but the outcome is a piece of art"
- *main(void)-this is the entry point and the void passed in is similar to a null parameter
- *
- *return(0) This indicates success in code execution
- */
+/**
+* Description - This Script is meant to print this statement with a new line
+* "with proper grammar, but the outcome is a piece of art"
+* main(void)-this is the entry point and the void passed in is similar to a null parameter
+* return(0) This indicates success in code execution
+*/
 
 
-int main(void){
+int main(void)
+{
 printf("with proper grammar, but the outcome is a piece of art,/n");
 }
