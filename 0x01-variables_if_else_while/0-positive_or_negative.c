776b7df91c4c4ee393326d35ab0056d6cbc8effb
Elijahdanie <elijahdanie@gmail.com>
2021-06-18 13:23:52 +0100

    fixed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,9 +22,9 @@ printf("%d is positive\n", n);
 }
 else if (n < 0)
 {
-printf("%d is negetive\n", n);
+printf("%d is negetive \n", n);
 }
-else if(n == 0)
+else if (n == 0)
 {
 printf("%d is zero\n", n);
 }
