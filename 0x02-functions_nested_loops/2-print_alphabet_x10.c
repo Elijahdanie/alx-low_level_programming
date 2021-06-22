6aa5be623d8abd189dba6efef1a6a02cd9f79d92
Elijahdanie <elijahdanie@gmail.com>
2021-06-22 20:54:53 +0100

    Add files

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,12 +4,12 @@
  *
  * Returm: Always success 0
  */
-void print_alphabet_x10 (void)
+void print_alphabet_x10(void)
 {
 	int i = 0, k = 0;
-	for (; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for(k = 97; k < 123; k++)
+		for (k = 97; k < 123; k++)
 		{
 			_putchar(k);
 		}
