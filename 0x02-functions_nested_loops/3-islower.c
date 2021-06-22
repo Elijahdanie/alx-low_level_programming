565a828ef70879296b6ff2ff8ad378119d1d7538
Elijahdanie <elijahdanie@gmail.com>
2021-06-22 23:06:00 +0100

    fixes

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,19 +2,19 @@
 
 /**
 * _islower - this checks if character is an alphabet or not
-* 
+*
 *Return: Always return 0, success
 */
 
 int _islower(int c)
 {
-    if (c >= 65 && c <= 90)
-    {
-        return (0);
-    }
-    else if (c >= 97 && c <= 122)
-    {
-        return (1);
-    }
-    return (0);
+if (c >= 65 && c <= 90)
+{
+return (0);
+}
+else if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
 }
