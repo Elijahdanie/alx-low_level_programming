1b0af193f268713798de246fbdf9e60c7c700131
Elijahdanie <elijahdanie@gmail.com>
2021-06-22 19:29:09 +0100

    fix

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,14 +1,10 @@
 #include "holberton.h"
 
 /**
-*_putchar- this is a function declaration of a file in holberton.h
-* char : this is the variable passed in
-*
 *main - Entry point for the program
 *
 *Return: Always Success 0
 */
-int _putchar(char);
 
 int main(void)
 {
