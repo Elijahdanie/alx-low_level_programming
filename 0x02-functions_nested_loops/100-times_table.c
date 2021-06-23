1f69699ac37c78c114ece262417c84586d2a39c0
Elijahdanie <elijahdanie@gmail.com>
2021-06-23 16:54:00 +0100

    pushed

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,7 +4,50 @@
  *
  */
 
-void print_times_table(int n)
+void print_times_table(int q)
 {
+	int i = 0, k = 0;
+    int m = q + 1;
+	for (i = 0; i < m; i++)
+	{
+		_putchar('0');
+		_putchar(44);
+		for (k = 1; k < m; k++)
+		{
+			int n = i * k;
 
+			if (k != 0)
+			{
+				_putchar(' ');
+			}
+			if (n > 9)
+			{
+				int first = n / 10;
+				int second = n % 10;
+                if(n > 99)
+                {
+                    int first = n / 10;
+				    int second = n % 10;
+                }
+
+				_putchar(first + '0');
+				_putchar(second + '0');
+				if (k != q)
+				{
+					_putchar(44);
+				}
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(n + '0');
+				if (k != q)
+				{
+					_putchar(44);
+				}
+			}
+		}
+		_putchar('\n');
+	}
 }
+
