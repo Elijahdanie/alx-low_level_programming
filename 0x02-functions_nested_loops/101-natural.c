f506d42ee8ad8d544852760fe53212efba5bc17d
Elijahdanie <elijahdanie@gmail.com>
2021-06-23 21:44:59 +0100

    fix 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,17 +7,18 @@
 
 int main(void)
 {
-    int i = 0, totalval = 0;
-    for (i = 0; i < 1024; i++)
-    {
-        if(i != 1024)
-        {
-            if (i % 3 == 0 || i % 5 == 0)
-            {
-                totalval += i;
-            }
-        }
-    }
-    printf("%d\n", totalval);
-    return (0);
+int i = 0, totalval = 0;
+
+for (i = 0; i < 1024; i++)
+{
+if (i != 1024)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+totalval += i;
+}
+}
+}
+printf("%d\n", totalval);
+return (0);
 }
