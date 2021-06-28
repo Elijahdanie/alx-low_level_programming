cf4b077f038f73a75e4756e09a83b5e82246b621
Elijahdanie <elijahdanie@gmail.com>
2021-06-28 21:58:33 +0100

    fixes

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,11 +11,11 @@ int length = 0;
 
 while (s[length])
 {
-    length++;
+length++;
 }
 for (length = length - 1; length >= 0; length--)
 {
-    _putchar(s[length]);
+_putchar(s[length]);
 }
 _putchar('\n');
 }
