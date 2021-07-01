1d2d2ae615bc3dd40cb8d18df4e664bd15d19d17
Elijahdanie <elijahdanie@gmail.com>
2021-07-01 04:07:48 -0700

    Fixes for betty styling

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,19 +1,20 @@
 #include "holberton.h"
 
 /**
- *char *leet(char *); - This reverses am integer array
+ *char *reverse_array - This reverses am integer array
  *@a: this is the pointer to the int array passed in
  *@n: this is the size of the int array
  */
 
 void reverse_array(int *a, int n)
 {
-int i = 0, temp = 0;
+	int i = 0, temp = 0;
 
-for (i = n - 1; i > (n / 2); i++)
-{
-temp = a[n - 1 - i];
-a[n - 1 - i] = a [i];
-a[i] = temp;
+	for (i = n - 1; i > (n / 2); i++)
+	{
+		temp = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = temp;
+	}
 }
-}
+
