d283a6fa9c2989ed00f4b00b4d1d13bf2be39485
Elijahdanie <elijahdanie@gmail.com>
2021-07-05 20:47:36 +0100

    fixed

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,19 +12,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	char *ptr = dest;
+	unsigned int i = 0;
 
-	n--;
-	while (*src)
+	for (i = 0; i < n; i++)
 	{
-		if (n > 0)
-		{
-			*dest++ = *src++;
-		}
-		else
-		{
-			break;
-		}
-		n--;
+		dest[i] = src[i];
 	}
 	return (ptr);
 }
