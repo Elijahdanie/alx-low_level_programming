9c1574d718b4b890af4193f1b5c69ae2c26422dc
Elijahdanie <elijahdanie@gmail.com>
2021-07-09 12:37:55 +0100

    fixes

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _atoi - function specified for static libraries
+ *
+ * @s: character passed in
+ *
+ * Return: Returns int
+ */
+
+
+int _atoi(char *s)
+{
+
+}
---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk -  searches a string for any of a set of bytes.
+ * @s: the string to check for occurence
+ * @accept: the set of bytes to look for in s
+ * Return: Returns a pointer to the byte in s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+char *finalptr = '\0';
+char *ptrcache = accept;
+int seen = 0;
+while (*s)
+{
+while (*accept)
+{
+if (*accept == *s)
+{
+seen = 1;
+break;
+}
+accept++;
+}
+if (seen == 1)
+{
+finalptr = s;
+break;
+}
+s++;
+accept = ptrcache;
+}
+return (finalptr);
+}
