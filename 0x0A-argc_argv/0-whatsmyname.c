caa7be47c93645cee6122b82def10ded75a0493e
Elijahdanie <elijahdanie@gmail.com>
2021-07-12 17:40:41 +0100

    fixes

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- *main-thie is the entry point for the program
+ *main-the is the entry point for the program
  *@argc:this is the number of command line arguments
  *@argv:this is an array that stores the command line arguments
- Return : returns 0 if success or 1 if error
+ *Return:returns 0 if success or 1 if error
 */
 
 int main(int argc, char *argv[])
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,7 +5,7 @@
  *main-thie is the entry point for the program
  *@argc:this is the number of command line arguments
  *@argv:this is an array that stores the command line arguments
- Return : returns 0 if success or 1 if error
+ *Return:returns 0 if success or 1 if error
 */
 
 int main(__attribute__((unused)) int argc, char *argv[])
