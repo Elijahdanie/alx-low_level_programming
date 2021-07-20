7a66d4c740a264329fb5fed026393e0b5ebf749e
Elijahdanie <elijahdanie@gmail.com>
2021-07-20 10:40:47 +0100

    edits

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #define SIZE 1024
 #endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #define ABS(x) ((x) > 0 ? (x) : ((x) * -1))
 #endif
