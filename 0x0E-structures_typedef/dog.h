7807ddc7bea50f47ad3aaa9c42eb765287076afb
Elijahdanie <elijahdanie@gmail.com>
2021-07-20 10:43:03 +0100

    fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,13 +16,10 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - Typedef for dog structure
- */
-typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
