2085a156c4acbca3fc05d0816973e0eae066bc89
Elijahdanie <elijahdanie@gmail.com>
2021-07-29 17:28:55 +0100

    betty edits

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,38 +11,34 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newnode;
-	unsigned int len = 0;
-	const char *strtemp;
+list_t *newnode;
+unsigned int len = 0;
+const char *strtemp;
 
-    if(str == NULL)
-    {
-        return (NULL);
-    }
+if (str == NULL)
+{
+return (NULL);
+}
+newnode = malloc(sizeof(list_t));
 
-    newnode = malloc(sizeof(list_t));
-    
-    if (newnode == NULL)
-    {
-        return (NULL);
-    }
+if (newnode == NULL)
+{
+return (NULL);
+}
+newnode->next = *head;
+newnode->str = strdup(str);
+if (newnode->str == NULL)
+{
+free(newnode);
+return (NULL);
+}
+strtemp = str;
+while (strtemp[len])
+{
+len++;
+}
+newnode->len = len;
 
-    newnode->next = *head;
-    newnode->str = strdup(str);
-    if (newnode->str == NULL)
-    {
-        free(newnode);
-        return (NULL);
-    }
-    strtemp = str;
-    while (strtemp[len])
-    {
-        len++;
-    }
-    newnode->len = len;
-    
-
-    *head = newnode;
-
-    return (newnode);
+*head = newnode;
+return (newnode);
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,12 +8,13 @@
  */
 void free_list(list_t *head)
 {
-    list_t *previous;
-    while (head)
-    {
-        previous = head;
-        head = head->next;
-        free(previous);
-        free(previous->str);
-    }
+list_t *previous;
+
+while (head)
+{
+previous = head;
+head = head->next;
+free(previous);
+free(previous->str);
+}
 }
