8649c1c4306d7bff106c204198e0ebdfb17bb1fb
Elijahdanie <elijahdanie@gmail.com>
2021-07-29 09:25:52 -0700

    fixes

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,13 +8,14 @@
 
 size_t print_list(const list_t *h)
 {
-	unsigned int numnodes = 0;
-    while (h != NULL)
-    {
-	    numnodes++;
-        printf("[%d] %s\n", h->len, h->str == NULL?"(nil)":h->str);
-        h = h->next;
-    }
-    return numnodes;
+	unsigned int numnodes;
+
+	while (h != NULL)
+	{
+		numnodes++;
+		printf("[%d] %s\n", h->len, h->str == NULL ? "(nil)" : h->str);
+		h = h->next;
+	}
+	return (numnodes);
 }
 
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,11 +7,13 @@
  */
 size_t list_len(const list_t *h)
 {
-    unsigned int numnodes = 0;
-    while (h != NULL)
-    {
-	    numnodes++;
-        h = h->next;
-    }
-    return numnodes;
+	unsigned int numnodes = 0;
+
+	while (h != NULL)
+	{
+		numnodes++;
+		h = h->next;
+	}
+	return (numnodes);
 }
+
