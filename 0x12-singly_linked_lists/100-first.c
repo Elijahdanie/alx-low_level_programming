2f1bb02d4792fe6115473d25c20164bb450d3b7e
Elijahdanie <elijahdanie@gmail.com>
2021-07-29 15:12:40 -0700

    final push

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,3 @@
-  
 #include <stdio.h>
 
 void print_before_main(void) __attribute__ ((constructor));
@@ -10,5 +9,7 @@ void print_before_main(void) __attribute__ ((constructor));
   */
 void print_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
+
