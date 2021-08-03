2f072ae1da3186de782753aa20df2fbf43498a48
Elijahdanie <elijahdanie@gmail.com>
2021-08-03 22:39:20 +0100

    fixed

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,7 +9,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *tmp, *tmpHead = *head;
+listint_t *tmp, *tmpHead = *head;
 	unsigned int node;
 
 	if (tmpHead == NULL)
