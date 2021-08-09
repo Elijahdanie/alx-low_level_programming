df5927f2b961b24a4155c20665e2aa6d17b1ec76
Elijahdanie <elijahdanie@gmail.com>
2021-08-10 00:12:35 +0100

    check this

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,23 +10,17 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-int charLength = 0;
 
-if (text_content == NULL)
-{
-return (0);
-}
-fd = open(text_content, O_RDWR | O_CREAT | O_TRUNC);
-if (fd == -1)
-{
-return (0);
-}
-while (text_content[charLength])
-{
-charLength++;
-}
+	if (!filename)
+		return (-1);
 
-write(fd, text_content, charLength);
-close(fd);
-return (1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+		write(fd, text_content, _strlen(text_content));
+
+	close(fd);
+	return (1);
 }
