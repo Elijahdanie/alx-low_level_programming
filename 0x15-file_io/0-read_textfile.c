3227abed492450ff8518291c4a377b1a26f2e0c3
Elijahdanie <elijahdanie@gmail.com>
2021-08-09 20:27:16 +0100

    edited and fixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,31 +9,23 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t textread;
-size_t fd;
+int fd, readed;
+	char *buff = malloc(sizeof(char *) * letters);
 
-char *buff;
-if (filename == NULL)
-{
-return (0);
-}
-buff = malloc(sizeof(char) * letters);
-if (buff == NULL)
-{
-return (0);
-}
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-{
-return (0);
-}
-textread = read(fd, buff, letters);
-if (textread == -1)
-{
-return (0);
-}
-write(STDOUT_FILENO, buff, textread);
-free(buff);
-close(fd);
-return (1);
+	if (!buff)
+		return (0);
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY, 0600);
+	if (fd == -1)
+		return (0);
+
+	readed = read(fd, buff, letters);
+	write(STDOUT_FILENO, buff, readed);
+
+	free(buff);
+	close(fd);
+	return (readed);
 }
