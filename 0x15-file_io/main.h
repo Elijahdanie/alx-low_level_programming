02191267a53add62b1829fd709c5b2466d4e34f3
Elijahdanie <elijahdanie@gmail.com>
2021-08-09 21:01:02 +0100

    fixed

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,3 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
 #include<fcntl.h>
 #include<unistd.h>
 #include<sys/types.h>
@@ -10,4 +8,3 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void processFileCopy(const char *srcfilename, const char *destfilename);
-#endif
