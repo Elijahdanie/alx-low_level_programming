3216943e611268389f058f5e25ffb92e1cf278ba
Elijahdanie <elijahdanie@gmail.com>
2021-08-10 11:16:46 +0100

    check

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,5 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #include<stdlib.h>
 
 int _strlen(char*);
@@ -5,3 +7,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void processFileCopy(const char *srcfilename, const char *destfilename);
+#endif
