c72f8c0cc59e33eaf8dad51acf64713907536b86
Elijahdanie <elijahdanie@gmail.com>
2021-10-08 22:11:52 +0100

    fixed errors

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,29 +1,36 @@
-#include "main.h"
+#include "hash_tables.h"
+/**
+ * hash_table_create - function creates a hash table
+ *
+ * @size: unsigned int size of array
+ *
+ * Return: ptr to new hash table, or NULL on error
+ */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht;
-    unsigned long int iterator = 0;
+	unsigned long int x = 0;
 
-    ht = malloc(sizeof(hash_node_t));
+	/* allocate memory for hash table, check if NULL */
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
-    if (ht == NULL)
-    {
-        return NULL;
-    }
-
-    ht->array = malloc(sizeof(hash_node_t *) * size);
-
-    if (ht->array == NULL)
-    {
-        free(ht);
-        return NULL;
-    }
-    
-    for (iterator = 0; iterator < size; iterator++)
-    {
-        ht->array[iterator] = NULL;
-    }
-    
-    return ht;
+	if (new_table == NULL)
+		return (NULL);
+	if (size < 1)
+		return (NULL);
+	/* allocate memory for  entries in hash table */
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	new_table->size = size;
+	/* set entries to NULL to be able to search properly */
+	while (x < size)
+	{
+		new_table->array[x] = NULL;
+		x++;
+	}
+	return (new_table);
 }
